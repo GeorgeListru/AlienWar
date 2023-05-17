370026f88e7362a45d2e42154ffb9d0af8e151e6
George <george.listru@gmail.com>
2023-05-17 16:58:42 +0300

    player, rocket, enemy1/2

---
+++ scenes/rocket.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@export var speed = 250
+@onready var visible_notifier = $VisibleOnScreenNotifier2D
+
+func _physics_process(delta):
+	global_position.x+=speed * delta
+	speed *= 1.1
+	if speed > 2000:
+		speed = 2000
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
